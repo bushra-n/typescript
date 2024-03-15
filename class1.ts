4699a43e42b92ee1c93a20dad758ecbe3c69e821
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-03-15 21:24:01 +0500

    Add files via upload

---
+++ class1.ts
@@ -0,0 +1,4 @@
+console.log('hello world');
+// We can add comment by pressing ctrl and /.In this way different developers communicate,working on same project.
+let message="hello to the people,around the globe working in the fied of AI";
+console.log(message);
---
+++ class4.ts
@@ -0,0 +1,27 @@
+let firstName:string='nasir';
+console.log(firstName)
+firstName='yusha ali khan'
+console.log(firstName)
+let isMarried:boolean=true
+console.log(isMarried)
+console.log(10+47)
+console.log(5+10/3*9)
+// post increment and decrement requires two steps coding;
+let e=5
+console.log(e++)
+console.log(e)
+console.log(e)
+console.log(e=e+4)
+let g=20
+console.log(g--)
+console.log(g)
+console.log(g=g-2)
+// pre increment and decrement requires one step coding
+let h=9
+console.log(++h)
+console.log(h=h+2)
+let r=2
+console.log(--r)
+console.log(r=r-2)
+const gravity=9.8
+console.log(gravity)
